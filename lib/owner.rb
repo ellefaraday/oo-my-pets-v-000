915b09eaf75fc96a9d520109ea715c22dc6a157b
Elle Faraday <faraday.elle@gmail.com>
2017-11-12 21:38:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,14 @@
 class Owner
   @@owners = []
   @@count = 0
-  attr_reader :species 
-  
+  attr_reader :species
+  attr_accessor :name, :pets
+
   def initialize(species)
     @@owners << self
     @@count += 1
     @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def self.reset_all
@@ -21,5 +23,25 @@ class Owner
     "I am a #{@species}."
   end
 
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+    fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+    cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+    dog
+  end
+
   
+
+
 end
