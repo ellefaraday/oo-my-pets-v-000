7d5d3710aa5455619d1b01d92e4f744b2617e17f
Elle Faraday <faraday.elle@gmail.com>
2017-11-12 21:33:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  @@owners = []
+  @@count = 0
+  attr_reader :species 
+  
+  def initialize(species)
+    @@owners << self
+    @@count += 1
+    @species = species
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self.count
+    @@count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  
 end
